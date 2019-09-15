655ceb2430008ed5e9cea01606667e60cd37b1bc
Jonah Brown <jswish02@gmail.com>
2019-09-15 21:10:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,11 +31,11 @@ def adding
                  }
  
 
-shipping_manifest = ["muskets" = 2]
+shipping_manifest = ["muskets"] = 2
 
   
 
-shipping_manifest = ["gun powder"= 4]
+shipping_manifest = ["gun powder"]= 4
 
 
 shipping_manifest
